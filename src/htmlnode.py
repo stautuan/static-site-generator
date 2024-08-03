e09160fd58f6cf2c4a8348244f768222961a5f9a
stautuan <stautuan89@gmail.com>
2024-08-03 17:59:09 -0400

    htmlnode class

---
+++ src/htmlnode.py
@@ -0,0 +1,28 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+
+node = HTMLNode("a", "Click me", None, {
+    "href": "https://www.google.com",
+    "target": "_blank",
+})
+print(node.props_to_html())
