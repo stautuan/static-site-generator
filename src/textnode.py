e825fb166364eb3b9125856c0c0904977fc04a1d
stautuan <stautuan89@gmail.com>
2024-08-03 17:14:06 -0400

    textnode unit test

--- src/textnode.py
+++ src/textnode.py
@@ -5,7 +5,7 @@ class TextNode:
         self.url = url
 
     def __eq__(self, other):
-        return self.tag == other.tag and self.text_type == other.text_type and self.url == other.url
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
 
     def __repr__(self):
         return f'TextNode({self.text}, {self.text_type}, {self.url})'
