57e8fcf791e757cd161046dd6bf8a4f46f437bc8
stautuan <stautuan89@gmail.com>
2024-08-03 19:55:41 -0400

    fix code

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -28,6 +28,7 @@ class LeafNode(HTMLNode):
     def to_html(self):
         if self.value == None:
             raise ValueError("Invalid HTML: no value")
+        if self.tag == None:
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
