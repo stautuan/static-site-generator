768148f85154225bfe37c5ebbf48f00260dcdd95
stautuan <stautuan89@gmail.com>
2024-08-03 23:24:32 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# static site generator
+
+It takes markdown files and turns them into a static website (HTML and CSS files)
--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,8 +33,6 @@ class LeafNode(HTMLNode):
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
-        
-
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
 
